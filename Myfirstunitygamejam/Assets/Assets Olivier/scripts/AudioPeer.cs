558339d4982b3ce4c7e8baf5c9317492eb4fe11c
vandhuynslagero <oliviervandhuynslager@gmail.com>
2019-12-07 09:52:15 +0100

    generate script to devide 512 samples into 8 bands to generate 8 Go instead of 512.

--- Myfirstunitygamejam/Assets/Assets Olivier/scripts/AudioPeer.cs
+++ Myfirstunitygamejam/Assets/Assets Olivier/scripts/AudioPeer.cs
@@ -7,6 +7,7 @@ public class AudioPeer : MonoBehaviour
 {
     private AudioSource _audioSource;
     public static float[] _samples = new float[512];
+    public static float[] _freqBand = new float[8];
     // Start is called before the first frame update
     void Start()
     {
@@ -18,11 +19,43 @@ public class AudioPeer : MonoBehaviour
     void Update()
     { 
         GetSpectrumAudioSource();
+        MakeFrequencyBands();
         
     }
 
     void GetSpectrumAudioSource()
     {
         _audioSource.GetSpectrumData(_samples, 0, FFTWindow.Blackman);
+    }
+
+    void MakeFrequencyBands()
+    {
+        /*
+         *  0 - 2 = 86 hertz
+         *  1 - 4 = 172 hertz - 
+         *  2 - 8 = 
+         */
+        
+        int count = 0;
+
+        for (int i = 0; i < 8; i++)
+        {
+            float average = 0;
+            int sampleCount = (int) Mathf.Pow(2, i) * 2;
+            if (i == 7)
+            {
+                sampleCount += 2;
+            }
+ 
+            for (int j = 0; j < sampleCount; j++)
+            {
+                average += _samples[count] + (count + 1);
+                count++;
+            }
+
+            average /= count;
+
+            _freqBand[i] = average * 10;
+        }
     }
 }
