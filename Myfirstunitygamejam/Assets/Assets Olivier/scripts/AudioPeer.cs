7c2ba4af533eb25459eecdca5773859dbbfb25e7
vandhuynslagero <oliviervandhuynslager@gmail.com>
2019-12-07 15:24:01 +0100

    no message

--- Myfirstunitygamejam/Assets/Assets Olivier/scripts/AudioPeer.cs
+++ Myfirstunitygamejam/Assets/Assets Olivier/scripts/AudioPeer.cs
@@ -8,6 +8,8 @@ public class AudioPeer : MonoBehaviour
     private AudioSource _audioSource;
     public static float[] _samples = new float[512];
     public static float[] _freqBand = new float[8];
+    public static float[] _bandBuffer = new float[8];
+    float[] _bufferDecrease = new float[8];
     // Start is called before the first frame update
     void Start()
     {
@@ -20,6 +22,7 @@ public class AudioPeer : MonoBehaviour
     { 
         GetSpectrumAudioSource();
         MakeFrequencyBands();
+        BandBuffer();
         
     }
 
@@ -27,6 +30,25 @@ public class AudioPeer : MonoBehaviour
     {
         _audioSource.GetSpectrumData(_samples, 0, FFTWindow.Blackman);
     }
+
+    void BandBuffer()
+    {
+        for (int g = 0; g < 8; ++g)
+        {
+            if (_freqBand[g] > _bandBuffer[g])
+            {
+                _bandBuffer[g] = _freqBand[g];
+                _bufferDecrease[g] = 0.005f;
+            }
+            if (_freqBand[g] < _bandBuffer[g])
+            {
+                _bandBuffer[g] -= _bufferDecrease[g];
+                _bufferDecrease[g] *= 1.2f;
+
+            }
+        }
+    }
+
 
     void MakeFrequencyBands()
     {
